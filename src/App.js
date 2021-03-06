2a9286e6082c4abed3c31cf2ec0bd8d10ee9a9b0
chicoumbetov <shyngysmaaan@gmail.com>
2021-03-06 10:02:20 +0100

    First commit. SignUp & Login from AWS amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+//import { withAuthenticator } from 'aws-amplify-react'
+
+Amplify.configure(awsconfig)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        <h1>App content</h1>
+        <h5>David-Julian j'ai réussi de faire authentification avec AWS</h5>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);

08a48d97f1b58866e4f62f04020e1680c72b32d2
chicoumbetov <shyngysmaaan@gmail.com>
2021-03-06 11:29:17 +0100

    Connect graphql, fetch, useState,useEffect, console.log array

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import './App.css';
-import Amplify from 'aws-amplify';
+import { useState, useEffect } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
 //import { withAuthenticator } from 'aws-amplify-react'
+import { listSongs } from './graphql/queries';
 
 Amplify.configure(awsconfig)
 
 function App() {
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    fetchSongs()
+    
+  }, [])
+
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs))
+      const songList = songData.data.listSongs.items
+      console.log('song list', songList);
+      setSongs(songList)
+    } catch (error) {
+      console.log('error on fetching songs', error)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <AmplifySignOut />
         <h1>App content</h1>
-        <h5>David-Julian j'ai réussi de faire authentification avec AWS</h5>
+        
       </header>
     </div>
   );
